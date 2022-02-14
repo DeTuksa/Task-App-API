a6530218a508e84580f871b4f74f7d7bf5f8c8fe
DeTuksa <39960305+DeTuksa@users.noreply.github.com>
2022-02-14 16:36:15 +0100

    added sorting and limit to fetch task api

--- src/routers/taskRoute.js
+++ src/routers/taskRoute.js
@@ -21,14 +21,26 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.next),
+                sort
+            }
         }).execPopulate()
         res.send({status: true, message: 'task retrieved successfully', tasks: req.user.tasks})
     } catch (error) {
